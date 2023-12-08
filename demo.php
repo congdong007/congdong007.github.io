36cd921e05c7c2278b4c20073f54f153a1860785
congdong007 <eric.congdong@gmail.com>
2023-12-07 23:04:36 -0500

    Site updated: 2023-12-07 23:03:50

--- demo.php
+++ demo.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
 echo "demo.php running..."
 $myfile = fopen("demo.txt", "a") or die("Unable to open file!");
 $txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"];
