37007d521cab541a1748af74e71f40880aa01ba6
congdong007 <eric.congdong@gmail.com>
2023-12-07 22:48:17 -0500

    Site updated: 2023-12-07 22:47:24

---
+++ demo.php
@@ -0,0 +1,9 @@
+<?php
+echo "demo.php running..."
+$myfile = fopen("demo.txt", "a") or die("Unable to open file!");
+$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"];
+fwrite($myfile, $txt);
+$txt = "***********************************\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
